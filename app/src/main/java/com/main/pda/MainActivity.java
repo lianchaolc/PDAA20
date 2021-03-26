9261e8fa7fff01f109ff8897c0c564baa567cb37
lianchao <lianchao@jpt-tj.com>
2021-03-26 18:15:20 +0800

    ce

--- app/src/main/java/com/main/pda/MainActivity.java
+++ app/src/main/java/com/main/pda/MainActivity.java
@@ -36,7 +36,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends PrivilegeActivity implements OnTouchListener {
-	// 主界面1
+	// 主界面12
 
 	ImageView home; // 主菜单
 	ImageView systemset; // 系统设置
