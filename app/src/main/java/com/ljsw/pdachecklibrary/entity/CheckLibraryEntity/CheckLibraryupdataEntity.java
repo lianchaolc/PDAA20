6624c8c86f8699e253cc20f8f3d6b13350785294
lianchao <lianchao@jpt-tj.com>
2021-08-27 14:55:03 +0800

    实体类

---
+++ app/src/main/java/com/ljsw/pdachecklibrary/entity/CheckLibraryEntity/CheckLibraryupdataEntity.java
@@ -0,0 +1,10 @@
+package com.ljsw.pdachecklibrary.entity.CheckLibraryEntity;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2021/8/26.
+ */
+
+//public class CheckLibraryupdataEntity implements Serializable {
+//}
