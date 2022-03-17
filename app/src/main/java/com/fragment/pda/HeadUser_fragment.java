43d3293ffdbfab51286609b93d3a4ebd3f66a353
lianchao <lianchao@jpt-tj.com>
2022-03-17 17:13:58 +0800

    顶部人员信息显示

--- app/src/main/java/com/fragment/pda/HeadUser_fragment.java
+++ app/src/main/java/com/fragment/pda/HeadUser_fragment.java
@@ -14,39 +14,43 @@ import android.widget.TextView;
 
 public class HeadUser_fragment extends Fragment {
 
-	TextView name1;
-	TextView name2;
+    TextView name1;
+    TextView name2;
 
-	private SystemLoginBiz systemLogin;
+    private SystemLoginBiz systemLogin;
 
-	public SystemLoginBiz getSystemLogin() {
-		return systemLogin == null ? new SystemLoginBiz() : systemLogin;
-	}
+    public SystemLoginBiz getSystemLogin() {
+        return systemLogin == null ? new SystemLoginBiz() : systemLogin;
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.head_user, null);
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.head_user, null);
+    }
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		name1 = (TextView) getActivity().findViewById(R.id.head_user1);
-		name2 = (TextView) getActivity().findViewById(R.id.head_user2);
+    @Override
+    public void onStart() {
+        super.onStart();
+        name1 = (TextView) getActivity().findViewById(R.id.head_user1);
+        name2 = (TextView) getActivity().findViewById(R.id.head_user2);
+        if (null != GApplication.getApplication().user.getOrganizationId()) {
 
-		if (GApplication.getApplication().user.getLoginUserId().equals("8")) {
-			name1.setText("押运员：" + GApplication.getApplication().user.getLoginUserName());
-			name2.setText("");
-		} else {
-			name1.setText(BankDoublePersonLogin.textname1);
-			name2.setText(BankDoublePersonLogin.textname2);
-		}
 
-	}
+            if (GApplication.getApplication().user.getLoginUserId().equals("8")) {
+                name1.setText("押运员：" + GApplication.getApplication().user.getLoginUserName());
+                name2.setText("");
+            } else {
+                name1.setText(BankDoublePersonLogin.textname1);
+                name2.setText(BankDoublePersonLogin.textname2);
+            }
+        } else {
+            System.out.print("空的");
+        }
+    }
 
 }
