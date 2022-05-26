4d13c7fca6ee0ec187971875f91489f44a407687
lianchao <lianchao@jpt-tj.com>
2022-05-26 11:16:21 +0800

    版本升级的代码添加网络装态判断

--- app/src/main/java/com/online/update/biz/GetPDA.java
+++ app/src/main/java/com/online/update/biz/GetPDA.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+
+import com.application.GApplication;
 import com.online.update.service.PDAVersionInfoPathService;
 
 public class GetPDA {
@@ -32,9 +34,15 @@ public class GetPDA {
 		@Override
 		public void run() {
 			Message m = Message.obtain();
+			String path="";
 			try {
+
 				// 获得配置文件路径
-				String path = new PDAVersionInfoPathService().getpath();
+				if(GApplication.netState.equals("13")){//  是3G
+				    path = new PDAVersionInfoPathService().getpath();
+				}else if (GApplication.netState.equals("11")){//  shi是wifi
+					path = new PDAVersionInfoPathService().getpathBYWiFi();
+				}
 
 				if (path != null && path != "") {
 					load = new LoadInfo();
@@ -59,6 +67,65 @@ public class GetPDA {
 			} catch (Exception e) {
 				e.printStackTrace();
 
+				m.what = -2;
+			} finally {
+				this.h.sendMessage(m);
+			}
+
+		}
+	}
+	public void getpathONlineyuan(Handler h, Context context) {
+		new Thread(new RunONlineyuan(h, context)).start();
+	}
+
+	class RunONlineyuan implements Runnable {
+		private Handler h;
+		private Context context;
+
+		RunONlineyuan() {
+		}
+
+		RunONlineyuan(Handler h, Context context) {
+			this.h = h;
+			this.context = context;
+		}
+
+		@Override
+		public void run() {
+			Message m = Message.obtain();
+			String path="";
+			try {
+
+				// 获得配置文件路径
+				if(GApplication.netState.equals("13")){//  是3G
+					path = new PDAVersionInfoPathService().getpath();
+				}else if (GApplication.netState.equals("11")){//  shi是wifi
+					path = new PDAVersionInfoPathService().getpathBYWiFi();
+				}
+
+				if (path != null && path != "") {
+					load = new LoadInfo();
+
+					Log.i("获得配置文件路径", path);
+					// 下载配置文件,并解析
+					boolean boo = load.loadInfo(path);
+					if (boo) {
+						VersionsCheckBiz version = new VersionsCheckBiz();
+
+						// 对比版本，如果有新版本
+						if (version.getVersionCode(this.context, VersionInfo.VERSION)) {
+							m.what = 98;
+							Log.i("有新版本", "有新版本");
+						} else {
+							m.what = 44;
+						}
+					}
+				} else {
+					m.what = 0;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+
 				m.what = -1;
 			} finally {
 				this.h.sendMessage(m);
@@ -66,5 +133,4 @@ public class GetPDA {
 
 		}
 	}
-
 }
