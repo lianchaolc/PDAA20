585d0c9aad8f14e56deff01d19043dfe2926a59b
lianchao <lianchao@jpt-tj.com>
2022-06-06 15:31:23 +0800

    区分有线和无线下的网络下  下载apk 地址

--- app/src/main/java/com/online/update/service/PDAVersionInfoPathService.java
+++ app/src/main/java/com/online/update/service/PDAVersionInfoPathService.java
@@ -25,4 +25,25 @@ public class PDAVersionInfoPathService {
 		// LR_TODO: 2020/6/12 16:22 liu_rui 测试更新下载代码
 //		return "http://test.afuiot.com:8080/upload/versionInfo.xml";
 	}
+
+	/***
+	 * 配合上线有线网络状态下的更新    将原有的getPDAVersionInfoPath 这个方法 改成有线的方式
+	 * @return
+	 * @throws Exception
+	 */
+	public  String getpathBYWiFi() throws Exception {
+
+		String methodName = "getPDAVersionInfoPath";
+		WebParameter[] param = new WebParameter[0];
+		System.out.println("获取配置文件路径getpathBYWiFi");
+
+		SoapObject soap = WebService.getSoapObject(methodName, param);
+		System.out.println("" + soap);
+		String params = soap.getProperty("params").toString();
+		return params.toString();
+
+		// LR_TODO: 2020/6/12 16:22 liu_rui 测试更新下载代码
+//		return "http://test.afuiot.com:8080/upload/versionInfo.xml";
+	}
+
 }
